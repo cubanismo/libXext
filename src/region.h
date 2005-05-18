3dde0d733cad58f01c3d667d7e6bc0dd6ca40608
Søren Sandmann Pedersen <sandmann@daimi.au.dk>
2005-05-18 18:50:23 +0000

    - Add build system for Xext

---
+++ src/region.h
@@ -0,0 +1,11 @@
+/*
+ * Dummy header to work around the fact that XShape.c does
+ *
+ *  #include "region.h"
+ *
+ * but the files is actually part of libX11 and installed in
+ * /usr/include/X11/region.h
+ *
+ */
+
+#include <X11/region.h>
